6d09fafd93e69e07bd3a329204f824826637a87b
york <york@duomai.com>
2020-07-26 09:10:31 +0800

    add ch01/read_and_show_img

---
+++ src/ch01/read_and_show_img.cc
@@ -0,0 +1,10 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cv::Mat image;
+    cout << "The image is " << image.rows << "x" << image.cols << endl;
+}
