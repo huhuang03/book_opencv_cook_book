fae73e348175e89647578dce6d4c1be82b25f62b
york <york@duomai.com>
2020-08-05 09:15:59 +0800

    up

--- src/ch04/color_histogram.hpp
+++ src/ch04/color_histogram.hpp
@@ -1,3 +1,19 @@
-class ColorHistogram {
+#ifndef _H_COLOR_HISTOGRAM
+#define _H_COLOR_HISTOGRAM
 
-}
+/**
+ * 三个通道的图像，分通道计算直方图
+ */
+class ColorHistogram {
+    private:
+    
+    public:
+    /**
+     * 设置直方图方块数
+     */
+    void setSize(int s1, int s2, int s3);
+    
+};
+
+
+#endif
