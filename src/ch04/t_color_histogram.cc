75d62bbd6fe68bb3e3bea01641b994dab2c82dc9
york <york@duomai.com>
2020-08-10 02:06:46 +0800

    up

---
+++ src/ch04/t_color_histogram.cc
@@ -0,0 +1,33 @@
+#include <opencv2_ex/opencv2_ex.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include "content_finder.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    auto img = cv::imread("waves.jpg");
+
+    if (img.empty()) {
+        cout << "read image failed." << endl;
+        return -1;
+    }
+
+    ColorHistogram hc;
+    auto imgRoi = img(cv::Rect(0, 0, 100, 45));
+    hc.setSize(8);
+    cv::Mat shist = hc.process(img);
+
+    ContentFinder finder;
+    finder.setHistogram(shist);
+    finder.setThreShold(0.05f);
+
+    cv::Mat rst = finder.find(img);
+    
+    cv::imshow("Image", img);
+    cv::imshow("Image1", rst);
+    cv::waitKey(0);
+}
